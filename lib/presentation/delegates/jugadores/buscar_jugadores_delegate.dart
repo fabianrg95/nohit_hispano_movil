248e2a65bc733e1547ac43217e9d61a01e3314ef
Fabian Mauricio Rodriguez Rodriguez <fabian.rg0801@gmail.com>
2024-02-28 18:27:02 -0500

    Ajustes sobre el splash y el icono

--- lib/presentation/delegates/jugadores/buscar_jugadores_delegate.dart
+++ lib/presentation/delegates/jugadores/buscar_jugadores_delegate.dart
@@ -1,7 +1,6 @@
 import 'dart:async';
 import 'package:animate_do/animate_do.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 import 'package:no_hit/infraestructure/dto/jugador/jugador_dto.dart';
 import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:no_hit/presentation/widgets/widgets.dart';
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
