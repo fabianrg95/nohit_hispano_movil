e19d9c60020fb6891eb12610e50467266ee1b918
Fabian Mauricio Rodriguez Rodriguez <fabian.rg0801@gmail.com>
2025-11-13 21:17:17 -0500

    Actualizacion dependencias y versiones

--- lib/infraestructure/providers/juegos/juegos_favoritos_local_provider.dart
+++ lib/infraestructure/providers/juegos/juegos_favoritos_local_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:no_hit/infraestructure/providers/hitless_repository_provider.dart';
 import 'package:no_hit/infraestructure/repositories/local_repository_impl.dart';
 
--- lib/infraestructure/providers/partidas/ultimas_partidas_provider.dart
+++ lib/infraestructure/providers/partidas/ultimas_partidas_provider.dart
@@ -1,4 +1,4 @@
-import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:flutter_riverpod/legacy.dart';
 import 'package:no_hit/config/helpers/utilidades.dart';
 import 'package:no_hit/domain/entities/entities.dart';
 import 'package:no_hit/infraestructure/dto/dtos.dart';
